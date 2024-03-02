0f01da23476e9ddb45aaa2e5e1a4c8f3cb94e588
pitbulek0 <piolesiak12@gmail.com>
2024-03-02 22:42:46 +0100

    fix: make scene responsive

--- src/containers/FullscreenCanvas.tsx
+++ src/containers/FullscreenCanvas.tsx
@@ -6,10 +6,11 @@ import * as THREE from 'three';
 import { EffectComposer } from 'three/addons/postprocessing/EffectComposer.js';
 import { RenderPass } from 'three/addons/postprocessing/RenderPass.js';
 import { UnrealBloomPass } from 'three/addons/postprocessing/UnrealBloomPass.js';
+import { FilmPass } from 'three/addons/postprocessing/FilmPass.js';
 import { OutputPass } from 'three/addons/postprocessing/OutputPass.js';
 
 function main(canvas: HTMLCanvasElement) {
-  const renderer = new THREE.WebGLRenderer({ canvas });
+  const renderer = new THREE.WebGLRenderer({ canvas, antialias: true });
   renderer.setPixelRatio(window.devicePixelRatio);
   renderer.setSize(window.innerWidth, window.innerHeight);
   renderer.toneMapping = THREE.ReinhardToneMapping;
@@ -43,10 +44,14 @@ function main(canvas: HTMLCanvasElement) {
 
   composer.addPass(new RenderPass(scene, camera));
   composer.addPass(
-    new UnrealBloomPass(new THREE.Vector2(window.innerWidth, window.innerHeight), 1.5, 2, 0),
+    new UnrealBloomPass(new THREE.Vector2(window.innerWidth, window.innerHeight), 0.75, 0, 0),
   );
+  composer.addPass(new FilmPass(3));
   composer.addPass(new OutputPass());
   renderer.toneMappingExposure = 4;
+
+  let t = 0,
+    color = new THREE.Color().setHSL(Math.random(), 0.5, 0.5);
 
   function update() {
     requestAnimationFrame(update);
@@ -54,6 +59,18 @@ function main(canvas: HTMLCanvasElement) {
     cube.rotation.x += 0.001;
     cube.rotation.y += 0.002;
     cube.rotation.z += 0.003;
+
+    if (t < 1) {
+      cube.material.color.lerp(color, t);
+      t += 0.001;
+    } else {
+      color = new THREE.Color().setHSL(Math.random(), 0.5, 0.5);
+      t = 0;
+    }
+
+    scene.scale.x = window.innerWidth > 922 ? 1.0 : window.innerWidth / 922;
+    scene.scale.y = window.innerWidth > 922 ? 1.0 : window.innerWidth / 922;
+    scene.scale.z = window.innerWidth > 922 ? 1.0 : window.innerWidth / 922;
 
     composer.render();
   }
